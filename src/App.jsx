0d54a72925d0fa2cc51ef26e3247042724c1083f
omarakamal <omaraakamal@gmail.com>
2026-02-08 19:13:28 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import { use, useState } from "react"
 function App() {
 
   const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
   function handleUsername(event){
     setUsername(event.target.value)
+  }
+
+  function handlePassword(event){
+    setPassword(event.target.value)
   }
   return (
     <div>
       <form>
         <label htmlFor="username">username:</label>
         <input value={username} onChange={handleUsername} id="username" type="text" />
+
+        <label htmlFor="">password</label>
+        <input value={password} onChange={handlePassword} type="password" />
         <button>Sign Up</button>
       </form>
     </div>
